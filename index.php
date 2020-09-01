ca3d9d20fb69d44bf2c278ddba192d0f66512a07
jian-lin <jian-lin@mail.chungyo.net>
2020-09-01 17:43:20 +0800

    ..

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php
-    require_once("connectDB.php");
-    //取得36h天氣，縣市
-    function get_weather36h()
-    {
-        $weather=curl_init();
-        curl_setopt($weather,CURLOPT_URL,"https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-CD094466-F0F5-46D5-B4CE-B55F5026618B&format=JSON");
-        curl_setopt($weather,CURLOPT_HEADER,false);
-        curl_setopt($weather,CURLOPT_RETURNTRANSFER,1);
+require_once("connectDB.php");
+//授權碼
+$Authorization="CWB-CD094466-F0F5-46D5-B4CE-B55F5026618B";
+global $Authorization;
+//存放相對應城市資源代碼
+$countryCode = array(
+    '宜蘭縣' => 'F-D0047-001',
+    '桃園市' => 'F-D0047-005',
+    '新竹縣' => 'F-D0047-009',
 
-        $w36h=curl_exec($weather);
-        curl_close($weather);
+);
 
-        return $w36h;
-    }
-      $obj_w36h=json_decode(get_weather36h());
-      echo ($obj_w36h->{"records"}->{"location"}[0]->{"locationName"});
-    //    echo $obj_w36h->{'result'}->{"records"}->{"location"}[0]->{"locationName"};
+//取得縣市
+function get_country()
+{
+    $weather = curl_init();
+    curl_setopt($weather, CURLOPT_URL, "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-CD094466-F0F5-46D5-B4CE-B55F5026618B&format=JSON&elementName=Wx&timeFrom=2020-09-02T12%3A00%3A00&timeTo=2020-09-02T12%3A00%3A00");
+    curl_setopt($weather, CURLOPT_HEADER, false);
+    curl_setopt($weather, CURLOPT_RETURNTRANSFER, 1);
+
+    $w36h = curl_exec($weather);
+    curl_close($weather);
+
+    return $w36h;
+}
+$obj_w36h = json_decode(get_country());
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,38 +45,47 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-sm bg-secondary navbar-dark fixed-top" style=" position: fixed; width:100%;  height:60px;">
-    <!-- Brand/logo -->
-    <p class="navbar-brand" style="text-align:center; margin-top:20px">小雞氣象局（｡･ө･｡）</p>
-</nav>
+    <nav class="navbar navbar-expand-sm   fixed-top" style=" position: fixed; width:100%;  height:60px; background-color: rgb(230, 218, 150)">
+        <!-- Brand/logo -->
+        <p class="navbar-brand" style="text-align:center; margin-top:20px">小雞氣象局（｡･ө･｡）</p>
+    </nav>
     <?php
     ?>
     <div class="container" style="margin-top: 60px;">
-            <div  class="select">
-                <form action="" method="post">
-                    <label for="country">縣市</label>
-                    <select name="" id="country">
-                        <?php
-                            $i=0;
-                            while($obj_w36h->{"records"}->{"location"}[$i]->{"locationName"}!=NULL){
-                        ?>
-                        <option value=""><?=$obj_w36h->{"records"}->{"location"}[$i]->{"locationName"}?></option>
+        <div class="select wrapper">
+            <form action="" method="post">
+                <!--選擇縣市-->
+                <label for="country">縣市</label>
+                <select name="" id="country" style="background-color:royalblue; color:seashell">
+                    <option id="country0" value="none">請選擇縣市</option>
+                    <?php
+                    $i = 0;
+                    while ($obj_w36h->{"records"}->{"location"}[$i]->{"locationName"} != NULL) {
+                    ?>
+                        <option id="country<?= $i + 1 ?>" value="<?= $obj_w36h->{"records"}->{"location"}[$i]->{"locationName"} ?>"><?= $obj_w36h->{"records"}->{"location"}[$i]->{"locationName"} ?></option>
 
-                        <?php
-                            $i++;
-                            }
-                        ?>
-                    </select>
-                    <label for="city">鄉鎮</label>
-                    <select name="" id="city">
-                            
-                    </select>
-                </form>
-            </div>
-            
+                    <?php
+                        $i++;
+                    }
+                    ?>
+                </select>
+                <label for="city" style="margin-left: 50px ;">鄉鎮</label>
+                <!--選擇鄉鎮-->
+                <select name="" id="city" style="background-color:royalblue; color:seashell">
+                    <script>
+                        $("#country").change(function() {
+                            alert($("#country").val());
+                            <?php
+
+                            ?>
+                        });
+                    </script>
+                </select>
+            </form>
+        </div>
+
 
     </div>
-   
 </body>
 
 </html>
